2c006592c205d60e85887a86d39af038046429d4
Cheenee <cheenee.colaste24@gmail.com>
2017-10-01 16:47:31 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaFeatures": {
+        "experimentalObjectRestSpread": true,
+        "jsx": true
+    },
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "windows"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-unused-vars": [1, {
+      "vars": "all",
+      "args": "none"
+    }],
+    // "strict":[
+    //   2,
+    //   "never"
+    // ],
+    // "react/jxs-uses-react":2,
+    // "react/jxs-uses-vars":2,
+    // "react/react-in-jsx-scope":2
+  }
+};
